9e7690709ab9c4759c6960920005f28eb2e37aca
Eden Aidan <1061551@hr.nl>
2025-04-22 01:05:38 +0200

    Updating the database migrations/columbs to the raspberry pi project

---
+++ Dissssssssssssssssssss/Models/JsonFile.cs
@@ -0,0 +1,7 @@
+public class JsonFile
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+}
