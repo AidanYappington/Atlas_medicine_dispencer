1392d61f5b015ac346f011caf986f8f88b58c79d
chiefD <105441682+AltaafDoekhie@users.noreply.github.com>
2025-06-01 22:01:53 +0200

    working on rasberry

---
+++ Atlas_medicine_dispencer-Revision-1(1)/MedicineDispencer/Program.cs
@@ -0,0 +1,31 @@
+using MedicineDispencer.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Register Compartments as a singleton
+builder.Services.AddSingleton<CompartmentsData>();
+builder.Services.AddSingleton<NotificationService>();
+builder.Services.AddSingleton<LogService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.Lifetime.ApplicationStopping.Register(() => LEDService.Dispose());
+app.Run();
