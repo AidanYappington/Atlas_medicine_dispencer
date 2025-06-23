55b6de271e1e6f48573a6597548e64eb1660a5e1
aym1279 <1040029@hr.nl>
2025-06-23 13:48:25 +0200

    added unit test for compartment creation

--- MedicineDispencer.Tests/UnitTest1.cs
+++ MedicineDispencer.Tests/UnitTest1.cs
@@ -1,10 +1,18 @@
-﻿namespace MedicineDispencer.Tests;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using MedicineDispencer;
+using Xunit;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void MedicijnCompartiment_Creation_Works()
     {
-
+        var comp = new MedicijnCompartiment("Paracetamol", "500mg", 10, new List<TimeSpan> { new(8, 0, 0) });
+        Assert.Equal("Paracetamol", comp.MedicijnNaam);
+        Assert.Equal("500mg", comp.Dosis);
+        Assert.Equal(10, comp.Voorraad);
+        Assert.Single(comp.DoseringstijdenPerDag);
     }
 }
